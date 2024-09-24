d49158b81f1045b41d884c3fd6ffef288cbd5f50
Jorge Chan <jorgeluis.chanchan@gmail.com>
2024-09-24 14:51:20 -0600

    Permite agregar el uso de los @inputs en el componente product

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  img = 'https://picsum.photos/640/640?r=' + Math.random();
+  @Input({required: true}) img: string = '';
+  @Input({required: true}) price: number = 0;
+  @Input({required: true}) title: string = '';
 }
