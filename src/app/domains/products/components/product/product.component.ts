ba8bc4a9686a3df14093837f85acb2a8568c5a44
Jorge Chan <jorgeluis.chanchan@gmail.com>
2024-09-26 10:04:39 -0600

    Permite agregar un ejemplo del uso de outputs en Angular

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,10 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
@@ -12,4 +12,11 @@ export class ProductComponent {
   @Input({required: true}) img: string = '';
   @Input({required: true}) price: number = 0;
   @Input({required: true}) title: string = '';
+
+  @Output() addToCart = new EventEmitter();
+
+  addToCartHandler() {
+    console.log('Click desde el hijo');
+    this.addToCart.emit('Hola este es un msg desde el hijo ' + this.title);
+  }
 }
