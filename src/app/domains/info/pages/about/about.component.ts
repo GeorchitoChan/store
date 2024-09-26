9aa57ae4a7626f09de2393704865a110c42df86b
Jorge Chan <jorgeluis.chanchan@gmail.com>
2024-09-26 16:10:27 -0600

    Permite agregar la librería wavesurfer para utilizarlo en el componente about y también configurar la carpeta assets

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CouterComponent } from "../../../shared/components/couter/couter.component";
-
+import { CouterComponent } from "./../../../shared/components/couter/couter.component";
+import { WaveAudioComponent } from "./../../../info/components/wave-audio/wave-audio.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, CouterComponent],
+  imports: [CommonModule, CouterComponent, WaveAudioComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
