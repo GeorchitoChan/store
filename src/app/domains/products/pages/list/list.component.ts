22704d8250bf583e46a137ad3835e6c4868caec1
Jorge Chan <jorgeluis.chanchan@gmail.com>
2024-09-27 10:33:46 -0600

    Permite agregar la interface de product y se rediseña el componente product

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,15 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 import { ProductComponent } from '../../components/product/product.component';
+import { Product } from '../../../shared/models/product.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initproducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 2',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=12',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 3',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=4',
+        creationAt: new Date().toISOString()
+      }
+    ];
+
+    this.products.set(initproducts);
+  }
+
   fromChild(event: string) {
     console.log('Estamos en el padre');
     console.log(event);
