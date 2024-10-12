4f07568d98d5c2505c5ad42b9d88a56a17b702f1
Jorge Chan <jorgeluis.chanchan@gmail.com>
2024-10-12 17:06:28 -0600

    Permite agregar la pagina no encontrado cuando se intenta acceder una ruta que no existe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
-import { ListComponent } from './domains/products/pages/list/list.component';
-import { AboutComponent } from './domains/info/pages/about/about.component';
+import { ListComponent } from '@products/pages/list/list.component';
+import { AboutComponent } from '@info/pages/about/about.component';
+import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,10 @@ export const routes: Routes = [
     {
         path: 'about',
         component: AboutComponent
+    },
+    // Siempre debe ir de último
+    {
+        path: '**',
+        component: NotFoundComponent
     }
 ];
