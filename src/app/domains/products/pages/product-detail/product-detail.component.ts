5e573d75eb4bfeae99d47eca16ad55cab1da549a
Jorge Chan <jorgeluis.chanchan@gmail.com>
2024-10-14 17:14:15 -0600

    Permite agregar la funcionalidad al servicio para obtener un solo producto por medio del id y se configuro el proyecto para obtener los parámetros como inputs y se renderizo la información del detalle del producto

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '@shared/models/product.model';
+import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
+  
+  @Input() id?: string;
+  private productService = inject(ProductService);
+  product = signal<Product | null>(null);
 
+  ngOnInit() {
+    if (this.id) {
+      this.productService.getSingleProduct(this.id)
+      .subscribe({
+        next: (product) => {
+          this.product.set(product);
+        }
+      })
+    }
+  }
 }
