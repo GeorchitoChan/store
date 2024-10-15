00263234315bdd4d788b897fa1feb7461a2d2d8d
Jorge Chan <jorgeluis.chanchan@gmail.com>
2024-10-14 18:19:45 -0600

    Permite agregar el servicio category para consultar en la API las categorias y mostrarlas en el componente list

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -6,6 +6,8 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "@shared/components/header/header.component";
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/models/category.model';
 
 @Component({
   selector: 'app-list',
@@ -16,10 +18,21 @@ import { ProductService } from '@shared/services/product.service';
 })
 export class ListComponent {
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
 
   ngOnInit() {
+    this.getProducts();
+    this.getCategories();
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+  }
+
+  private getProducts() {
     this.productService.getProduct()
     .subscribe({
       next: (products) => {
@@ -31,7 +44,15 @@ export class ListComponent {
     })
   }
 
-  addToCart(product: Product) {
-    this.cartService.addToCart(product);
+  private getCategories() {
+    this.categoryService.getAll()
+    .subscribe({
+      next: (data) => {
+        this.categories.set(data);
+      },
+      error: () => {
+
+      }
+    })
   }
 }
