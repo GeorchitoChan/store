6d61bbecf75f3c4ffb7b6d695ecb4f35973a87e7
Jorge Chan <jorgeluis.chanchan@gmail.com>
2024-10-16 10:59:20 -0600

    Permite agregar el filtro por categoría de los productos mediante el uso de URL params

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject, signal } from '@angular/core';
-
+import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
 import { ProductComponent } from '@products/components/product/product.component';
 import { Product } from '@shared/models/product.model';
 import { CommonModule } from '@angular/common';
@@ -12,7 +12,7 @@ import { Category } from '@shared/models/category.model';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent, HeaderComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -22,10 +22,14 @@ export class ListComponent {
   private cartService = inject(CartService);
   private productService = inject(ProductService);
   private categoryService = inject(CategoryService);
+  @Input() category_id?: string;
 
   ngOnInit() {
-    this.getProducts();
     this.getCategories();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.getProducts()
   }
 
   addToCart(product: Product) {
@@ -33,7 +37,7 @@ export class ListComponent {
   }
 
   private getProducts() {
-    this.productService.getProduct()
+    this.productService.getProduct(this.category_id)
     .subscribe({
       next: (products) => {
         this.products.set(products);
