713217a3a9cbef574edfa4cee0397609678962cd
Jorge Chan <jorgeluis.chanchan@gmail.com>
2024-10-16 14:11:13 -0600

    Permite migrar la sintaxis antigua de angular a la nueva versión

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
 import { RouterLinkWithHref } from '@angular/router';
 import { ProductComponent } from '@products/components/product/product.component';
 import { Product } from '@shared/models/product.model';
-import { CommonModule } from '@angular/common';
+
 import { HeaderComponent } from "@shared/components/header/header.component";
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
@@ -12,7 +12,7 @@ import { Category } from '@shared/models/category.model';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent, HeaderComponent, RouterLinkWithHref],
+  imports: [ProductComponent, HeaderComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
